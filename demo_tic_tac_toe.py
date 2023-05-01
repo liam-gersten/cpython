70fe44ff63b273433f7916522d886326e42c66b9
liam-gersten <gerstenliam@gmail.com>
2023-05-01 13:35:43 -0400

    Demo stashed changes

--- demo_tic_tac_toe.py
+++ demo_tic_tac_toe.py
@@ -1,149 +1,99 @@
 import math
-from project_demo import use_turtle
-
-turtle = use_turtle
-
-def tic_tac_toe():
-    pass
 
 
-def init_turtle():
-    print("\ninit turtle")
-    yertle = turtle.Turtle()
-    yertle.ht()
-    yertle.speed(4)
-    yertle.up()
-    return yertle
+class TicTacToe():
+    def __init__(self, turtle_module):
+        self.turtle = turtle_module
+        self.occupied = [[False, False, False] for _ in range(3)]
+        self.ecks = [[False, False, False] for _ in range(3)]
+        self.oh = [[False, False, False] for _ in range(3)]
+        self.circle = True
+        self.count = 0
 
 
-def draw_grid():
-    print("\ndrawing grid")
-    for y in range(80, 200, 80):
-        for x in range(80, 200, 80):
-            yertle = init_turtle()
-            yertle.setpos(0, y)
-            yertle.down()
-            yertle.setheading(0)
-            yertle.forward(240)
-            yertle.up()
-            yertle.setpos(x, 0)
-            yertle.down()
-            yertle.setheading(90)
-            yertle.forward(240)
+    def init_turtle(self):
+        yertle = self.turtle.Turtle()
+        yertle.ht()
+        yertle.speed(0)
+        yertle.up()
+        return yertle
 
 
-def setup_board():
-    print("\nsetup board")
-    for y in range(40, 280, 80):
-        for x in range(40, 280, 80):
-            yertle = init_turtle()
-            yertle.showturtle()
-            yertle.shape("square")
-            yertle.shapesize(4, 4, 4)
-            yertle.color("#FFFFFF")
-            yertle.goto(x, y)
-            yertle.onclick(mark)
-    draw_grid()
+    def draw_grid(self):
+        for y in range(80, 200, 80):
+            for x in range(80, 200, 80):
+                yertle = self.init_turtle()
+                yertle.setpos(0, y)
+                yertle.down()
+                yertle.setheading(0)
+                yertle.forward(240)
+                yertle.up()
+                yertle.setpos(x, 0)
+                yertle.down()
+                yertle.setheading(90)
+                yertle.forward(240)
 
 
-# def mark_win_row(i, c="pink"):
-#     yertle = init_turtle()
-#     yertle.setpos(0, i * 80 + 40)
-#     yertle.setheading(0)
-#     yertle.down()
-#     yertle.fd(240)
+    def setup_board(self):
+        for y in range(40, 280, 80):
+            for x in range(40, 280, 80):
+                yertle = self.init_turtle()
+                yertle.showturtle()
+                yertle.shape("square")
+                yertle.shapesize(4, 4, 4)
+                yertle.color("#FFFFFF")
+                yertle.goto(x, y)
+                yertle.onclick(self.mark)
+        self.draw_grid()
 
 
-# def mark_win_col(i, c="pink"):
-#     yertle = init_turtle()
-#     yertle.setpos(i * 80 + 40, 0)
-#     yertle.down()
-#     yertle.setheading(90)
-#     yertle.fd(240)
+    def draw_circle(self, x, y):
+        radius = 20
+        yertle = self.init_turtle()  # Creating the turtle with this function = error
+        yertle.color("red")
+        yertle.setpos(x, y-radius)
+        yertle.down()
+        yertle.showturtle()  # Remove this later, only used for demonstration
+        yertle.circle(radius)  # Error occurs here, when the turtle should draw.
+        self.circle = False
+        self.count += 1
 
 
-# def mark_win_diag(i, c="pink"):
-#     yertle = init_turtle()
-#     yertle.setpos(0, 0)
-#     yertle.setheading(45)
-#     yertle.down()
-#     yertle.forward(240 * math.sqrt(2))
-#     yertle.up()
-#     yertle.setpos(240, 0)
-#     yertle.setheading(135)
-#     yertle.down()
-#     yertle.forward(240 * math.sqrt(2))
+    def draw_x(self, x, y):
+        for angle_heading in range(45, 405, 90):
+                global count
+                mark_size = 70
+                yertle = self.init_turtle()
+                yertle.color("blue")
+                yertle.showturtle()
+                yertle.setpos(x,y)
+                yertle.setheading(angle_heading)
+                yertle.down()
+                yertle.forward(mark_size/2)
+                self.circle = True
+                self.count += 1
 
 
-def draw_circle(x, y):
-    print("\ndrawing circle")
-    global count
-    global circle
-    radius = 20
-    yertle = init_turtle()  # Creating the turtle with this function = error
-    yertle.color("red")
-    yertle.setpos(x, y-radius)
-    yertle.down()
-    yertle.showturtle()  # Remove this later, only used for demonstration
-    yertle.circle(radius)  # Error occurs here, when the turtle should draw.
-    circle = False
-    count += 1
+    def mark(self, x, y):
+        column = int(x // 80)
+        row = int(y // 80)
+        if self.circle and not self.occupied[row][column]:
+            self.draw_circle(x, y)
+            self.oh[row][column] = True
+            self.occupied[row][column] = True
+        elif not self.circle and not self.occupied[row][column]:
+            self.draw_x(x, y)
+            self.ecks[row][column] = True
+            self.occupied[row][column] = True
+        else:
+            self.turtle.textinput("BAD CLICK!", "A SHAPE OCCUPIES THAT SPACE! CLICK CANCEL TO DISMISS")
+        if self.count == 9:
+            pass
 
 
-def draw_x(x, y):
-    print("\ndraw x")
-    for angle_heading in range(45, 405, 90):
-            global count
-            global circle
-            mark_size = 70
-            yertle = init_turtle()
-            yertle.color("blue")
-            yertle.showturtle()
-            yertle.setpos(x,y)
-            yertle.setheading(angle_heading)
-            yertle.down()
-            yertle.forward(mark_size/2)
-            circle = True
-            count += 1
-
-
-def mark(x, y):
-    global circle
-    global occupied
-    column = int(x // 80)
-    row = int(y // 80)
-    if circle and not occupied[row][column]:
-        draw_circle(x, y)
-        oh[row][column] = True
-        occupied[row][column] = True
-    elif not circle and not occupied[row][column]:
-        draw_x(x, y)
-        ecks[row][column] = True
-        occupied[row][column] = True
-    else:
-        turtle.textinput("BAD CLICK!", "A SHAPE OCCUPIES THAT SPACE! CLICK CANCEL TO DISMISS")
-    if count == 9:
-        pass
-
-
-def main():
-    print("\nmain")
-    turtle.setup(width=680, height=680)
-    turtle.screensize(240, 240, "cyan")
-    wn = turtle.Screen()
-    wn.title('tic-tac-toe')
-    setup_board()
-
-
-circle = True
-
-ecks = [[False, False, False], [False, False, False], [False, False, False]]
-
-oh = [[False, False, False], [False, False, False], [False, False, False]]
-
-occupied = [[False, False, False], [False, False, False], [False, False, False]]
-
-count = 0
-
-main()
-turtle.TK.mainloop()
+    def main(self):
+        self.turtle.setup(width=680, height=680)
+        self.turtle.screensize(240, 240, "cyan")
+        wn = self.turtle.Screen()
+        wn.title('tic-tac-toe')
+        self.setup_board()
--- project_demo.py
+++ project_demo.py
@@ -1,18 +1,19 @@
-import Lib.turtle as turtle
-import Lib.old_turtle as old_turtle
+import time
 
 
-def drawStar(left: int, color: str) -> None:
-    turtle.color(color, "yellow")
-    current_pos = turtle.pos()
+def drawStar(left: int, color: str, turtle_mod) -> None:
+    turtle_mod.color(color, "yellow")
+    current_pos = turtle_mod.pos()
     while True:
-        turtle.forward(200)
-        turtle.left(left)
-        if abs(turtle.pos() - current_pos) < 1:
+        turtle_mod.forward(200)
+        turtle_mod.left(left)
+        if abs(turtle_mod.pos() - current_pos) < 1:
             break
 
 
 def demo_teleport(teleport_func) -> None:
+    import Lib.turtle as turtle_mod_A
+    turtle = turtle_mod_A.Turtle()
     turtle.color("red", "yellow")
     turtle.begin_fill()
     turtle.speed(10)
@@ -20,37 +21,49 @@ def demo_teleport(teleport_func) -> None:
     lefts = [130, 170, 140]
     coords = [(-250, -150), (-250, 150)]
     for i in range(3):        
-        drawStar(lefts[i], colors[i])
+        drawStar(lefts[i], colors[i], turtle)
         if i != 2:
             x, y = coords[i]
-            teleport_func(x, y)
+            if teleport_func == "teleport":
+                kwargs = {"x": x, "y": y}
+                turtle.teleport(**kwargs)
+            else:
+                turtle.goto(x, y)
     turtle.end_fill()
-    turtle.done()
+    turtle_mod_A.onclick(turtle_mod_A.done)
+    turtle_mod_A.mainloop()
 
 
 def show_teleport_before() -> None:
-    demo_teleport(turtle.goto)
+    try:
+        demo_teleport("goto")
+    except:
+        pass
 
 
 def show_teleport_after() -> None:
-    demo_teleport(turtle.teleport)
+    demo_teleport("teleport")
 
 
 def show_circle_before() -> None:
-    global use_turtle
-    use_turtle = old_turtle
-    import demo_tic_tac_toe
+    import Lib.old_turtle as old_turtle
+    from demo_tic_tac_toe import TicTacToe
+    ttt = TicTacToe(old_turtle)
+    ttt.main()
+    old_turtle.TK.mainloop()
 
 
 def show_circle_after() -> None:
-    global use_turtle
-    use_turtle = old_turtle
-    import demo_tic_tac_toe
+    import Lib.turtle as turtle_mod_C
+    from demo_tic_tac_toe import TicTacToe
+    ttt = TicTacToe(turtle_mod_C)
+    ttt.main()
+    turtle_mod_C.TK.mainloop()
 
 
 if __name__ == "__main__":
-    # show_teleport_before()
-    # show_teleport_after()
-    # show_circle_before()
+    show_teleport_before()
+    show_teleport_after()
+    show_circle_before()
     show_circle_after()
 
